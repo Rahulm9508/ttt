da2092e9c4595dae4d1f1e953c65d2466f942f7a
mayanksit <158296692+mayanksit@users.noreply.github.com>
2024-09-17 23:18:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ API_ID = getenv("API_ID", "27575225")
 API_HASH = getenv("API_HASH", "203a119993b61575dbab46e966c94fc8")
 sudo_group =  -1002478234192
 sudo_user = 1877735614
-log_channel = -1002433001335
+log_channel = -1002166157110
 
 
