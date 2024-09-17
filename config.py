14a7f047d1b1eec505c492665b7e825a6d1ab9ef
mayanksit <158296692+mayanksit@users.noreply.github.com>
2024-09-17 23:09:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from os import getenv
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7235097064:AAH1P1LrbAZd1uG8bn-5g3m8uN0rY39Pmlo")
-API_ID = getenv("API_ID", "23108855")
-API_HASH = getenv("API_HASH", "ee97c1084e29530c019b13c5778ab2db")
-sudo_group =  -1002175016234
-sudo_user = 7185229231
-log_channel = -1002129401270
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7205186382:AAHVkB8X6iO8I0vKnEkPFWL8iJt_LJh2p5E")
+API_ID = getenv("API_ID", "27575225")
+API_HASH = getenv("API_HASH", "203a119993b61575dbab46e966c94fc8")
+sudo_group =  -1002478234192
+sudo_user = 1877735614
+log_channel = -1002433001335
 
 
