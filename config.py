b64be8680e38c4f824f9e86a1ebbeece8b5531a2
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-09-22 10:21:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from os import getenv
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7912142463:AAHzdINXhcJfsZJvkPlSyKlGIPJ4JGtImIM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7417717621:AAFibMWs3uejBzUPJrljDww0zPq7NyQQo1M")
 API_ID = getenv("API_ID", "27575225")
 API_HASH = getenv("API_HASH", "203a119993b61575dbab46e966c94fc8")
-sudo_group =  -1002478234192
-sudo_user = 1877735614
-log_channel = -1002129401270
+sudo_group =  -1002116155974
+sudo_user = 5498931783
+log_channel = -1002116155974
 
 
